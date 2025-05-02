a9b7f17ae4768fbf066e2c321edee8f4eedf9e46
Xuebing Hou <xuebinghou60@gmail.com>
2025-05-02 15:38:35 -0700

    edit

--- projects/projects.js
+++ projects/projects.js
@@ -11,12 +11,19 @@ renderProjects(projects, projectsContainer, 'h2');
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // 准备数据
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 // 创建 arc 生成器
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
 
 arcData.forEach((d, idx) => {
@@ -24,4 +31,14 @@ arcData.forEach((d, idx) => {
     .append('path')
     .attr('d', arcGenerator(d))
     .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // 用变量传颜色
+    .attr('class', 'legend-item')            // 加 class 用于样式
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
