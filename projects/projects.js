c7aac1085b322641bbef388b5fe603eb26c69407
Xuebing Hou <xuebinghou60@gmail.com>
2025-05-02 17:18:17 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 const projects = await fetchJSON('../lib/projects.json');
 const titleElement = document.querySelector('.projects-title');
@@ -8,21 +10,20 @@ if (titleElement) {
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let rolledData = d3.rollups(
+  projects,
+  v => v.length,    // 每年的项目数量
+  d => d.year       // 按 year 分组
+);
 
-// 准备数据
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let data = rolledData.map(([year, count]) => ({
+  label: year,
+  value: count,
+}));
+
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-// 创建 arc 生成器
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
 let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
 
@@ -42,3 +43,4 @@ data.forEach((d, idx) => {
     .attr('class', 'legend-item')            // 加 class 用于样式
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
+
