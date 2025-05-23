959d5f442e5fd878ceaa1a1f696e6f14b5e42dbb
Xuebing Hou <xuebinghou60@gmail.com>
2025-05-23 11:12:04 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -383,32 +383,6 @@ function renderItems(startIndex) {
     .style('top', (_, idx) => `${idx * ITEM_HEIGHT}px`);
 }
 
-// Step 3.2: Generate commit text
-d3.select('#scatter-story')
-  .selectAll('.step')
-  .data(commits)
-  .join('div')
-  .attr('class', 'step')
-  .html(
-    (d, i) => `
-		On ${d.datetime.toLocaleString('en', {
-      dateStyle: 'full',
-      timeStyle: 'short',
-    })},
-		I made <a href="${d.url}" target="_blank">${
-      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
-    }</a>.
-		I edited ${d.totalLines} lines across ${
-      d3.rollups(
-        d.lines,
-        (D) => D.length,
-        (d) => d.file,
-      ).length
-    } files.
-		Then I looked over all I had made, and I saw that it was very good.
-	`,
-  );
-
 async function main() {
   data = await loadData();
   commits = processCommits(data);
@@ -494,5 +468,31 @@ async function main() {
       .style('background', d => fileTypeColors(d.type));
   });
 }
+
+// Step 3.2: Generate commit text (added at the bottom)
+d3.select('#scatter-story')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+      On ${d.datetime.toLocaleString('en', {
+        dateStyle: 'full',
+        timeStyle: 'short',
+      })},
+      I made <a href="${d.url}" target="_blank">${
+        i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+      }</a>.
+      I edited ${d.totalLines} lines across ${
+        d3.rollups(
+          d.lines,
+          (D) => D.length,
+          (d) => d.file,
+        ).length
+      } files.
+      Then I looked over all I had made, and I saw that it was very good.
+    `,
+  );
 
 main();
